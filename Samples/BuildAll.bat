149ed2c78fff0d82bbd67516fd0156faedb8878d
Eli Aloni <elix22@gmail.com>
2020-12-06 21:29:46 +0200

    build 64-bit

--- Samples/BuildAll.bat
+++ Samples/BuildAll.bat
@@ -2,8 +2,8 @@ set "PATH=c:\windows\microsoft.net\framework\v4.0.30319\"
 
 call Clear.bat
 
-csc /target:exe /out:01_HelloWorld.exe /reference:DotNetBindings.dll /platform:x86 01_HelloWorld\*.cs *.cs
-csc /target:exe /out:04_StaticScene.exe /reference:DotNetBindings.dll /platform:x86 04_StaticScene\*.cs *.cs
-csc /target:exe /out:34_DynamicGeometry.exe /reference:DotNetBindings.dll /platform:x86 34_DynamicGeometry\*.cs *.cs
+csc /target:exe /out:01_HelloWorld.exe /reference:DotNetBindings.dll /platform:x64 01_HelloWorld\*.cs *.cs
+csc /target:exe /out:04_StaticScene.exe /reference:DotNetBindings.dll /platform:x64 04_StaticScene\*.cs *.cs
+csc /target:exe /out:34_DynamicGeometry.exe /reference:DotNetBindings.dll /platform:x64 34_DynamicGeometry\*.cs *.cs
 
 pause
